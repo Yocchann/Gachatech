3df99c2d0b3910a7daa2b1542eef8fe38d5fbd15
Swift <Swift>
2020-07-24 10:22:38 +0900

    Initial Commit

---
+++ Gachatech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gachatech
+//
+//  Created by 加島慈久 on 2020/07/24.
+//  Copyright © 2020 加島慈久. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
